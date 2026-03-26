5ebb3c898c594e1d1589ffbc345f91d45a506af9
suparn <suparngautam@zapbuild.com>
2026-03-26 09:59:02 +0530

    unexpected runtime errors with useing ? where user thinks there can eror does not get the property or object key

--- day3/src/components/auth/ProtectedRoute.jsx
+++ day3/src/components/auth/ProtectedRoute.jsx
@@ -4,15 +4,17 @@ import { Navigate } from 'react-router-dom'
 const ProtectedRoute = ({children, allowedRoles}) => {
 
   // login from token/localstorage/api
-const user = {login:true,role:'admin'}
-
+  const user = {login:true,role:'admin'}
+  
+  // suppse you don not get the keys
+// const user= undefined
 // 401
-if(!user.login){
+if(!user?.login){
 return <Navigate to="/login" replace/>
 }
 
 //403
-if(user.role!=='admin'){
+if(user?.role!=='admin'){
 return <Unauthorized/>
 }
 
